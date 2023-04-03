dd370788e452e1c9c0f14d6dbea22022774258fa
RASantos92 <rasbms81608@gmail.com>
2023-04-03 16:02:22 -0500

    4/3 fixed arrays Dynamic arrays type casting

--- Test.java
+++ Test.java
@@ -1,11 +1,51 @@
+import java.util.ArrayList;
 public class Test{
     public static void main(String[] args){
         VoiceMessageUtil voiceMessageUtil = new VoiceMessageUtil();
 
-        System.out.println(voiceMessageUtil.greeting());
-        System.out.println(voiceMessageUtil.greeting("Shreyas"));
-        System.out.println(voiceMessageUtil.greeting("Shreyas",54));
-        System.out.println(voiceMessageUtil.getCurrentDate());
-        voiceMessageUtil.getCurrentDateAsDate();
+        // System.out.println(voiceMessageUtil.greeting());
+        // System.out.println(voiceMessageUtil.greeting("Shreyas"));
+        // System.out.println(voiceMessageUtil.greeting("Shreyas",54));
+        // System.out.println(voiceMessageUtil.getCurrentDate());
+        // voiceMessageUtil.getCurrentDateAsDate();
+
+        //----------------- fixed arrays--------------------
+
+        // int[] messagesPerDay = new int[5]; // [5,5,5,0,0]
+        // messagesPerDay[0] = 5;
+        // messagesPerDay[1] = 5;
+        // messagesPerDay[2] = 5;
+
+        int[] messagesPerDay = {5,4,6};
+
+        String[] messages = {"String1", "String2", "String3"};
+
+        System.out.println(voiceMessageUtil.totalMessages(messagesPerDay));
+
+
+        voiceMessageUtil.printMessages(messages);
+
+        //--------------- Dynamic arrays------------------
+
+        ArrayList<String> todoList = new ArrayList<String>();
+        todoList.add("Finish Alfred Bot");
+        todoList.add("Finish cafe bussiness logic");
+        todoList.add("Read ahead for oop");
+
+        voiceMessageUtil.printMessages(todoList);
+
+        int days = 7;
+        double costOfLiving = 40.54;
+
+        // implicit casting
+        double totalCost = days * costOfLiving;// const: double; day: int => double (no loss of data so java will implicitly convert int to double)
+        System.out.println(totalCost);
+
+        // explicitly casting
+        int roundedTotalCost = days * (int) costOfLiving;
+        System.out.println(roundedTotalCost);
+        String test = "40";
+        
+        System.out.println((int) Integer.valueOf(test));
     }
 }
--- VoiceMessageUtil.java
+++ VoiceMessageUtil.java
@@ -1,4 +1,5 @@
 import java.util.Date;
+import java.util.ArrayList;
 public class VoiceMessageUtil{
     public String greeting(){
         return "hello you";
@@ -11,6 +12,9 @@ public class VoiceMessageUtil{
     public String greeting(String name, Integer age){
         return "hello "  + name + " you age is " + age;
     }
+    public String greeting(int age){
+        return "hello you age: "  + age;
+    }
 
     public String getCurrentDate(){
         Date today = new Date();
@@ -22,5 +26,39 @@ public class VoiceMessageUtil{
         System.out.println(today);
     }
 
+    public int totalMessages(int[] intArr){
+        int output = 0;
+        for(int i = 0; i< intArr.length; i++){
+            output += intArr[i];
+        }
+        return output;
+    }
 
+    public void printMessages(String[] msgArray){
+        // traditional forloop
+
+        // for( int i = 0 ; i<msgArray.length; i++){
+        //     System.out.println(msgArray[i]);
+        // }
+
+        //enhanced forloop
+
+        for(String message : msgArray){
+            System.out.println(message);
+        }
+
+    }
+
+    public void printMessages(ArrayList<String> msgArrayList){
+        // traditional forloop
+        for(int i = 0; i<msgArrayList.size(); i++){
+            System.out.println(msgArrayList.get(i));
+        }
+
+        //enhanced forloop
+
+        for(String message : msgArrayList){
+            System.out.println(message);
+        }
+    }
 }
